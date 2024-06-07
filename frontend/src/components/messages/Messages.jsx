7d1c9144893915647aa1cd5769237245220a4df9
damian dev <inamida16@gmail.com>
2024-06-08 01:41:00 +0300

    socket io implennted and app ready for launch

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -2,10 +2,11 @@ import { useEffect, useRef } from 'react';
 import useGetMessages from '../../hooks/useGetMessages';
 import MessageSkeleton from '../skeletons/MessageSkeleton';
 import Message from './Message';
+import useListenMessages from '../../hooks/useListenMessages';
 
 const Messages = () => {
     const { messages, loading } = useGetMessages();
-
+    useListenMessages();
     const lastMessageRef = useRef();
 
     useEffect(() => {
@@ -32,3 +33,26 @@ const Messages = () => {
     );
 };
 export default Messages;
+
+// STARTER CODE SNIPPET
+// import Message from "./Message";
+
+// const Messages = () => {
+// 	return (
+// 		<div className='px-4 flex-1 overflow-auto'>
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 		</div>
+// 	);
+// };
+// export default Messages;
